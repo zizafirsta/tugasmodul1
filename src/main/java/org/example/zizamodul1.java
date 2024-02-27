455f1188085274ede038c2c9a34bc77b19037d08
zizafirsta <firstadewi14@gmail.com>
2024-02-27 09:36:06 +0700

    first commit

--- src/main/java/org/example/zizamodul1.java
+++ src/main/java/org/example/zizamodul1.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class zizatugas1modul1 {
+public class zizamodul1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
--- src/main/java/org/example/zizamodul1final.java
+++ src/main/java/org/example/zizamodul1final.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class zizatugas1modul1 {
+public class zizamodul1final {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
